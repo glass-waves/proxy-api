107877b49f9cf45a67593979ae7c089dd047ed1d
Dylan White <dylan.whitej@gmail.com>
2021-02-26 15:32:47 -0800

    initial commit

---
+++ data/dataUtils.js
@@ -0,0 +1,37 @@
+const formatLocation = (locData) => {
+    const firstCity = locData[0];
+    return {
+        formatted_query: firstCity.display_name,
+        latitude: firstCity.lat,
+        longitude: firstCity.lon
+    }
+}
+
+const formatWeather = (weatherData) => {
+
+    const dataArr = weatherData.body.data;
+    const mungedDataArr = dataArr.map(day => {
+        const correctDate = new Date(day.ts * 1000);
+        return {
+            forecast: day.weather.description,
+            time: correctDate 
+        }
+    })
+    return mungedDataArr.slice(0, 7);
+}
+
+const parseReviewData = (reviewData) => {
+    const parsedData = reviewData.body.businesses.map(business => {
+        return {
+            name: business.name,
+            image_url: business.image_url,
+            price: business.price,
+            rating:business.rating,
+            url: business.url,
+        }
+    })
+    return parsedData.slice(0,7);
+}
+
+
+module.exports = {formatLocation, formatWeather, parseReviewData};
